046ed9ae39da11c1ac248aa1bf41a1bbf0d0e7a1
llhyuan <lhyuan.liu21@icloud.com>
2023-02-21 20:48:52 +0100

    project setup

---
+++ news-homepage/src/component/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <div>
+      <img src='../../assets/images/logo.svg' alt='logo'></img>
+      <ul>
+        <li>Home</li>
+        <li>New</li>
+        <li>Popular</li>
+        <li>Trending</li>
+        <li>Categories</li>
+      </ul>
+    </div>
+  )
+}
