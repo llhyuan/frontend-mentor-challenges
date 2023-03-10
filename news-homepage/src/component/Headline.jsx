10b764bc157020f52fd41c9101da091284281b1a
llhyuan <lhyuan.liu21@icloud.com>
2023-03-10 09:48:04 +0100

    project finished

--- news-homepage/src/component/Headline.jsx
+++ news-homepage/src/component/Headline.jsx
@@ -1,20 +1,31 @@
 import headlinePhoto from '../assets/images/image-web-3-desktop.jpg'
+import headlinePhotoMobile from '../assets/images/image-web-3-mobile.jpg'
 import '../scss/_Headline.scss'
 
 function Headline() {
   return (
-      <div className='headline'>
+    <div className='headline'>
+      <picture>
+        <source
+          srcset={headlinePhotoMobile}
+          media='(max-width: 500px)'
+        />
         <img src={headlinePhoto} alt='headline story photo' />
-        <article>
-          <h1>The Bright Futrue of Web 3.0</h1>
-          <section>
-            <p>
-              We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?
-            </p>
-            <a className='button' href='#'>Read More</a>
-          </section>
-        </article>
-      </div>
+      </picture>
+      <article>
+        <h1>The Bright Futrue of Web 3.0</h1>
+        <section>
+          <p>
+            We dive into the next evolution of the web that claims to put the
+            power of the platforms back into the hands of the people. But is it
+            really fulfilling its promise?
+          </p>
+          <a className='button' href='#'>
+            Read More
+          </a>
+        </section>
+      </article>
+    </div>
   )
 }
 
--- news-homepage/src/component/Nav.jsx
+++ news-homepage/src/component/Nav.jsx
@@ -1,18 +1,49 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
 import logo from '../assets/images/logo.svg'
+
+import { useState } from 'react'
+
 import '../scss/_Nav.scss'
 
 function Nav() {
+  const [menuVisibility, setMenuVisibility] = useState(false)
   return (
-      <div className='nav'>
+    <div className='nav'>
+      <div className='nav__mobile'>
         <img src={logo} alt='logo' />
-        <ul id='nav'>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">News</a></li>
-          <li><a href="#">Popular</a></li>
-          <li><a href="#">Trending</a></li>
-          <li><a href="#">Categories</a></li>
-        </ul>
+        <FontAwesomeIcon
+          className='menu-toggle'
+          icon={faBars}
+          onClick={() => setMenuVisibility(!menuVisibility)}
+        />
       </div>
+      <ul className={!menuVisibility && 'menu-hide'}>
+        {menuVisibility && (
+          <FontAwesomeIcon
+            className='menu-toggle'
+            icon={faXmark}
+            onClick={() => setMenuVisibility(!menuVisibility)}
+          />
+        )}
+        <li>
+          <a href='#'>Home</a>
+        </li>
+        <li>
+          <a href='#'>News</a>
+        </li>
+        <li>
+          <a href='#'>Popular</a>
+        </li>
+        <li>
+          <a href='#'>Trending</a>
+        </li>
+        <li>
+          <a href='#'>Categories</a>
+        </li>
+      </ul>
+    </div>
   )
 }
 
